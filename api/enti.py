0080326fdef2e3ae82f91b5973f1aaae6bf52d51
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2024-08-30 11:58:37 +0200

    [P4ADEV-942] added new header for broker id (#9)

--- api/enti.py
+++ api/enti.py
@@ -1,13 +1,15 @@
 import requests
 
 from config.configuration import settings
+from config.configuration import secrets
 
 
 def get_enti_list(token, logo_mode: str = 'hash', cod_ipa_ente: str = None):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         params={
             'logoMode': logo_mode,
@@ -21,7 +23,8 @@ def get_ente_details(token, ente_id: int):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/{ente_id}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -31,7 +34,8 @@ def get_ente_details_public(token, ente_id: int):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.public}/enti/{ente_id}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -53,7 +57,8 @@ def post_insert_ente(token,
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/ente/insert',
         headers={
             'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         json={
             'codIpaEnte': cod_ipa_ente,
@@ -88,7 +93,8 @@ def post_update_ente(token,
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/{ente_id}/update',
         headers={
             'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         json={
             'mygovEnteId': ente_id,
@@ -111,7 +117,8 @@ def get_anagrafica_stati_ente(token):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/anagraficaStati',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -121,7 +128,8 @@ def get_tipi_ente(token):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/tipiEnte',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -131,7 +139,8 @@ def get_funzionalita_ente(token, ente_id: int):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/funzionalita/{ente_id}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -141,7 +150,8 @@ def get_registro_funzionalita(token, ente_id: int, funzionalita: str):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/funzionalita/{ente_id}/{funzionalita}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -151,7 +161,8 @@ def get_activate_funzionalita(token, funzionalita_id: int):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/funzionalita/activate/{funzionalita_id}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -161,7 +172,8 @@ def get_deactivate_funzionalita(token, funzionalita_id: int):
     return requests.get(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/funzionalita/deactivate/{funzionalita_id}',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         timeout=settings.default_timeout
     )
@@ -171,7 +183,8 @@ def post_save_logo(token, ente_id: int, file):
     return requests.post(
         f'{settings.api.base_path.payhub}/{settings.api.path_root.operatore_admin}/enti/{ente_id}/saveLogo',
         headers={
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            settings.BROKER_ID_HEADER: secrets.ente.intermediario_id
         },
         files={
             'file': file
