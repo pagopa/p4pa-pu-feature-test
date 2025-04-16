d37e8574d68fa7249a3f33688fe1da78179007bb
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-04-16 11:19:41 +0200

    set up for testing PU 2.0

--- util/utility.py
+++ util/utility.py
@@ -2,12 +2,9 @@ from config.configuration import secrets
 
 
 def get_user_id(user: str) -> str:
-    if user == 'Amministratore Globale':
-        return secrets.user_info.admin_global.user_id
-    elif user == 'Amministratore Ente':
-        return secrets.user_info.admin_ente.user_id
-    elif user == 'Operatore':
-        return secrets.user_info.operator.user_id
+    if user == 'Admin org with GPD':
+        return secrets.user_info.admin_org_gpd.user_id
+    elif user == 'Admin org with ACA':
+        return secrets.user_info.admin_org_aca.user_id
     else:
         raise ValueError
-
