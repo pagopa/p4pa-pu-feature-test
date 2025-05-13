d18628aca8578b3c86814e8308f8616630c7e583
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-05-13 12:05:41 +0200

    [P4ADEV-2696] payment of single installment

---
+++ api/classifications.py
@@ -0,0 +1,20 @@
+import requests
+
+from config.configuration import secrets, settings
+
+
+def get_classification(token, organization_id: int, iuv: str, iur: str,
+                       last_classification_date_from: str, last_classification_date_to: str):
+    return requests.get(
+        url=f'{secrets.internal_base_url}{settings.api.ingress_path.classifications}/organization/{organization_id}/classifications/treasured',
+        headers={
+            'Authorization': f'Bearer {token}'
+        },
+        params={
+            'lastClassificationDateFrom': last_classification_date_from,
+            'lastClassificationDateTo': last_classification_date_to,
+            'iuv': iuv,
+            'iur': iur
+        },
+        timeout=settings.default_timeout
+    )
