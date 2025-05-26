d807a781960e8d4bd40f7c248856122281dda804
pelliccm <matteo.pelliccione@nttdata.com>
2025-05-26 17:25:41 +0200

    [P4ADEV-2934] step_verify_presence_debt_position_in_gpd

--- bdd/features/debt_positions.feature
+++ bdd/features/debt_positions.feature
@@ -10,3 +10,13 @@ Feature: An organization creates a debt position
     Then the debt position is in status unpaid
     And the notice is present in ACA archive in status valid
     And the check of debt position expiration is scheduled
+
+  @gpd
+  Scenario: Organization interacting with GPD creates a simple debt position
+    Given organization interacting with GPD
+    And a new debt position of type TEST
+    And payment option 1 with single installment of 100 euros with due date set in 2 days
+    When the organization creates the debt position
+    Then the debt position is in status unpaid
+    And the notice is present in GPD archive in status valid
+    And the check of debt position expiration is scheduled
--- bdd/steps/gpd_aca_step.py
+++ bdd/steps/gpd_aca_step.py
@@ -1,6 +1,6 @@
 from behave import then
 
-from api.gpd_aca import get_debt_position_on_aca
+from api.gpd_aca import get_debt_position_on_aca, get_debt_position_on_gpd
 from bdd.steps.utils.debt_position_utility import retrieve_iuv_list
 from config.configuration import secrets
 
@@ -16,3 +16,17 @@ def step_verify_presence_debt_position_in_aca(context, status):
         res = get_debt_position_on_aca(org_fiscal_code=org_fiscal_code, iuv=iuv)
         assert res.status_code == 200
         assert res.json()['status'] == status.upper()
+
+
+@then("the notice is present in GPD archive in status {status}")
+@then("the notices are present in GPD archive in status {status}")
+def step_verify_presence_debt_position_in_gpd(context, status):
+    org_fiscal_code = secrets.organization.gpd.fiscal_code
+
+    debt_position = context.debt_position
+
+    for po in debt_position.payment_options:
+        for installment in po.installments:
+            res = get_debt_position_on_gpd(org_fiscal_code=org_fiscal_code, iupd_pagopa=installment.iupd_pagopa)
+            assert res.status_code == 200
+            assert res.json()['status'] == status.upper()
