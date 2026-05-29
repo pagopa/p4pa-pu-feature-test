927602685f5a84417b9c72ef582188bacad25070
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-05-29 16:54:23 +0200

    P4ADEV-4160-added-error-details-to-failed-feature-tests: fix traceparent log

--- bdd/environment.py
+++ bdd/environment.py
@@ -3,4 +3,5 @@ from bdd.steps.utils import utility
 
 def before_scenario(context, scenario):
     context.traceparent = utility.generate_traceparent()
-    print(f"[TRACE_PARENT] Scenario {scenario.name} with trace parent: {context.traceparent}")
+    print(f"""\n---------------------------------------------------------------------------------
+           \n[TRACE_PARENT] Scenario {scenario.name} with trace parent: {context.traceparent}""")
